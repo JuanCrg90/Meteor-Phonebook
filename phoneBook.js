9c9ef1be33b64caad3f478ff890e85a795c3ea14
Juan Carlos Ruiz <juancarlosruiz14@gmail.com>
2014-12-12 23:44:21 -0600

    added template, helpers and events

--- phoneBook.js
+++ phoneBook.js
@@ -1,23 +1,51 @@
+
+PhoneBook = new Mongo.Collection("phonebook");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+
+  Template.main.helpers({
+    phonebook: function () {
+      return PhoneBook.find();
+  }
+});
+
+  Template.main.events({    
+    "keypress #address": function(event){
+        if(event.charCode =='13'){
+            var name = $('#name').val();
+            var phone = $('#phone').val();
+            var address = $('#address').val();
+
+        Meteor.call("addRow", name,phone,address);
+
+        $('#name').val("");
+        $('#phone').val("");
+        $('#address').val("");
     }
-  });
+    }
+
+    });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-  });
+});
 }
+
+Meteor.methods({
+  addRow: function(name,phone,address)
+  {
+    if(name!="" && phone!="" && address!="")
+    {
+      PhoneBook.insert({
+        name:name,
+        phone:phone,
+        address:address
+    });
+  }
+}
+
+});
